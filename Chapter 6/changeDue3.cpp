908a4d2c41f638e08ff0d963a7c5d7bfc63d1962
WChan95 <59454205+WChan95@users.noreply.github.com>
2022-04-07 16:33:52 -0700

    finished 6.3

---
+++ Chapter 6/changeDue3.cpp
@@ -0,0 +1,141 @@
+// Wilson Chan
+// Programming Exercise 6.34 - Making Change, v.3.0
+
+#include <string>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int cashPayment;
+    int amountTendered;
+
+    while (true)
+    {
+        cout << "Cash payment amount: ";
+        cin >> cashPayment;
+        if (cashPayment < 0)
+        {
+            cin.fail();
+            cin.clear();
+            cin.ignore();
+            cout << "Error, cash payment must not be zero or negative." << endl;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    while (true)
+    {
+        cout << "Tendered amount ";
+        cin >> amountTendered;
+        if (amountTendered < cashPayment)
+        {
+            cin.fail();
+            cin.clear();
+            cin.ignore();
+            cout << "Error, amount tendered must be greater than or equal to cash payment." << endl;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    int changeDue = amountTendered - cashPayment;
+    cout << "Change due: " << changeDue << endl;
+
+    int hundredThousands = changeDue / 100000;
+    changeDue = changeDue % 100000;
+
+    int tenThousands = changeDue / 10000;
+    changeDue = changeDue % 10000;
+
+    int fiveThousands = changeDue / 5000;
+    changeDue = changeDue % 5000;
+
+    int thousand = changeDue / 1000;
+    changeDue = changeDue % 1000;
+
+    int fiveHundreds = changeDue / 500;
+    changeDue = changeDue % 500;
+
+    int hundred = changeDue / 100;
+    changeDue = changeDue % 100;
+
+    int fifty = changeDue / 50;
+    changeDue = changeDue % 50;
+
+    int twenty = changeDue / 20;
+    changeDue = changeDue % 20;
+
+    int ten = changeDue / 10;
+    changeDue = changeDue % 10;
+
+    int five = changeDue / 5;
+    changeDue = changeDue % 5;
+
+    int two = changeDue / 2;
+    changeDue = changeDue % 2;
+
+    int one = changeDue / 1;
+    changeDue = changeDue % 1;
+
+    if (hundredThousands != 0)
+    {
+        cout << "this many hundred thousand dollar bills: " << hundredThousands << endl;
+    }
+
+    if (tenThousands != 0)
+    {
+        cout << "this many ten thousand dollar bills: " << tenThousands << endl;
+    }
+    if (fiveThousands != 0)
+    {
+        cout << "this many five thousand dollar bills: " << fiveThousands << endl;
+    }
+    if (thousand != 0)
+    {
+        cout << "this many thousand dollar bills: " << thousand << endl;
+    }
+    if (fiveHundreds != 0)
+    {
+        cout << "this many five hundred dollar bills: " << fiveHundreds << endl;
+    }
+    if (hundred != 0)
+    {
+        cout << "this many hundred dollar bills: " << hundred << endl;
+    }
+
+    if (fifty != 0)
+    {
+        cout << "this many fifty dollar bills: " << fifty << endl;
+    }
+    if (twenty != 0)
+    {
+        cout << "this many twenty dollar bills: " << twenty << endl;
+    }
+    if (ten != 0)
+    {
+
+        cout << "this many ten dollar bills: " << ten << endl;
+    }
+    if (five != 0)
+    {
+
+        cout << "this many five dollar bills: " << five << endl;
+    }
+    if (two != 0)
+    {
+        cout << "this many two dollar bills: " << two << endl;
+    }
+    if (one != 0)
+    {
+        cout << "this many one dollar bills: " << one << endl;
+    }
+}
